df8b76ed03eb51fbc5e2ad7e02c1332dc816900a
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-04-08 19:06:30 +0200

    add studio section in main

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ export default {
 
     <div id="site-main">
 
+        <!-- Jumbotron section -->
         <section class="jumbo">
             <div class="container d-flex">
                 <h6>CINEMATO STUDIO</h6>
@@ -22,7 +23,68 @@ export default {
             </div>
 
         </section>
-        <section></section>
+
+
+        <section class="studio p-section">
+            <div class="section-title">
+                <h6>PELICULA STUDIO</h6>
+                <h2>THIS IS WHAT WE DO</h2>
+            </div>
+            <div class="container">
+                <div class="row d-flex">
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (14).svg" alt="">
+                        </div>
+
+                        <h4>ARRAY OF EQUIPMENT</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (16).svg" alt="">
+                        </div>
+                        <h4>NEW TECHNOLOGIES</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (18).svg" alt="">
+                        </div>
+                        <h4>VERSITILE ACTORS</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (15).svg" alt="">
+                        </div>
+                        <h4>TOP DIRECTORS</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (17).svg" alt="">
+                        </div>
+                        <h4>MINUTE EDITING</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (19).svg" alt="">
+                        </div>
+                        <h4>VERSITILE ACTORS</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
         <section></section>
         <section></section>
         <section></section>
@@ -98,64 +160,110 @@ a {
     margin: 0 auto;
 }
 
+.p-section {
+    padding: 3rem;
+}
+
 /* #endregion Utilities */
 
-.jumbo {
-    height: 30rem;
-    position: relative;
+#site-main {
 
-    &::before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-image: url(/img/jumbotron.webp);
-        background-position: center;
-        background-size: cover;
-        background-repeat: no-repeat;
-        filter: brightness(50%);
+    margin: 0 5px;
 
-
-    }
-
-    &>div {
+    .jumbo {
+        height: 30rem;
         position: relative;
-        z-index: 100;
-        flex-direction: column;
-        color: var(--cine--lightest);
-        justify-content: center;
-        height: 100%;
 
+        &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-image: url(/img/jumbotron.webp);
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            filter: brightness(50%);
 
-        & h1 {
-
-            font-size: 3rem;
-            word-spacing: 5px;
-            letter-spacing: 2px;
-            line-height: 60px;
-        }
-
-        & h6 {
-            letter-spacing: 2px;
-            word-spacing: 5px;
 
         }
 
+        &>div {
+            position: relative;
+            z-index: 100;
+            flex-direction: column;
+            color: var(--cine--lightest);
+            justify-content: center;
+            height: 100%;
 
-        & button {
-            background-color: var(--cine--lightest);
-            border: none;
-            padding: 0.5rem 1rem;
-            font-size: 10px;
-            font-weight: bold;
-            width: fit-content;
-            margin-top: 1rem;
 
+            & h1 {
+
+                font-size: 3rem;
+                word-spacing: 5px;
+                letter-spacing: 2px;
+                line-height: 60px;
+            }
+
+            & h6 {
+                letter-spacing: 2px;
+                word-spacing: 5px;
+
+            }
+
+
+            & button {
+                background-color: var(--cine--lightest);
+                border: none;
+                padding: 0.5rem 1rem;
+                font-size: 10px;
+                font-weight: bold;
+                width: fit-content;
+                margin-top: 1rem;
+
+            }
         }
+
+
     }
 
+    .studio {
+        background-color: var(--cine--light);
+
+        .section-title {
+            text-align: center;
+
+            & h2 {
+                margin: 0.5rem 0;
+            }
+        }
+
+        .row {
+            padding: 1rem 0;
+            flex-wrap: wrap;
+        }
+
+        .col {
+            width: calc(100%/12 * 4);
+            flex-direction: column;
+            align-items: center;
+            gap: 1rem;
+            justify-content: space-between;
+            padding-top: 3rem;
+
+            .top {
+                height: 5.5rem;
+                width: 5rem;
+            }
+
+            & p {
+                text-align: center;
+            }
+        }
+
+    }
 
 }
 </style>
