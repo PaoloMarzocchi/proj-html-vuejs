bed68b0d06cb41f233ed5fbd56d7a1ff12a62eea
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-04-09 15:47:08 +0200

    add main component and change section classes in id

---
+++ src/components/AppMain.vue
@@ -0,0 +1,346 @@
+<script>
+export default {
+    name: 'AppMain',
+}
+</script>
+
+
+<template>
+    <div id="site-main">
+
+        <!-- Jumbotron section -->
+        <section id="jumbo">
+            <div class="container">
+                <div class="text-box d-flex">
+                    <h6>CINEMATO STUDIO</h6>
+                    <h1>ACTION AND <br> INSPIRE PEOPLE</h1>
+                    <button class="btn-light">READ MORE</button>
+                </div>
+            </div>
+
+        </section>
+        <!-- /Jumbotron section -->
+
+        <!-- Studio section -->
+        <section id="studio" class="p-section">
+            <div class="section-title">
+                <h6>PELICULA STUDIO</h6>
+                <h2>THIS IS WHAT WE DO</h2>
+            </div>
+            <div class="container">
+                <div class="row d-flex">
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (14).svg" alt="">
+                        </div>
+
+                        <h4>ARRAY OF EQUIPMENT</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (16).svg" alt="">
+                        </div>
+                        <h4>NEW TECHNOLOGIES</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (18).svg" alt="">
+                        </div>
+                        <h4>VERSITILE ACTORS</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (15).svg" alt="">
+                        </div>
+                        <h4>TOP DIRECTORS</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (17).svg" alt="">
+                        </div>
+                        <h4>MINUTE EDITING</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+                    <div class="col d-flex">
+                        <div class="top">
+                            <img src="/img/image (19).svg" alt="">
+                        </div>
+                        <h4>VERSITILE ACTORS</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, laboriosam.</p>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- /Studio section -->
+
+        <!-- About us section -->
+        <section id="about">
+            <div class="container d-flex">
+                <div class="text-box d-flex">
+                    <h6>ABOUT US</h6>
+                    <h2>HIGH QUALITY CINEMA</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, repellendus dolore
+                        distinctio cumque neque voluptatibus eius reiciendis sunt non tenetur quasi incidunt deserunt
+                        aliquid sed? Reiciendis dolores similique officia aliquid.
+                    </p>
+                    <button class="btn-light">READ MORE</button>
+
+                </div>
+            </div>
+
+        </section>
+        <!-- /About us section -->
+
+        <!-- Watchlist section -->
+        <section id="watchlist" class="p-section">
+
+            <div class="section-title">
+                <h6>YOUR WATCHLIST</h6>
+                <h2>BEST HITS MOVIES</h2>
+            </div>
+            <div class="container">
+                <div class="row d-flex">
+                    <div class="col d-flex">
+                        <img src="/img/Gallery-01.jpg" alt="">
+                        <h4>SEVENTEENTH SUMMER</h4>
+                        <span>Commercial, Music Video</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/Gallery-02.jpg" alt="">
+                        <h4>SEVENTEENTH SUMMER</h4>
+                        <span>Commercial, Music Video</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/Gallery-03.jpg" alt="">
+                        <h4>SEVENTEENTH SUMMER</h4>
+                        <span>Commercial, Music Video</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/Gallery-04.jpg" alt="">
+                        <h4>SEVENTEENTH SUMMER</h4>
+                        <span>Commercial, Music Video</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/Gallery-05.jpg" alt="">
+                        <h4>SEVENTEENTH SUMMER</h4>
+                        <span>Commercial, Music Video</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/Gallery-01.jpg" alt="">
+                        <h4>SEVENTEENTH SUMMER</h4>
+                        <span>Commercial, Music Video</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- /Watchlist section -->
+
+        <!-- Statistics section -->
+        <section id="stats">
+            <div class="container">
+                <div class="row d-flex">
+                    <div class="col d-flex">
+                        <h2>158</h2>
+                        <h6>SATISFIED CLIENTS</h6>
+                    </div>
+                    <div class="col d-flex">
+                        <h2>3 +</h2>
+                        <h6>OUR PROJECTS</h6>
+                    </div>
+                    <div class="col d-flex">
+                        <h2>1.906</h2>
+                        <h6>CUP OF COFFEE</h6>
+                    </div>
+                    <div class="col d-flex">
+                        <h2>2</h2>
+                        <h6>OUR AWARDS</h6>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- /Statistics section -->
+
+        <!-- Team section -->
+        <section id="team" class="p-section">
+            <div class="section-title">
+                <h6>THE PERICUROS</h6>
+                <h2>MEET THE TEAM</h2>
+            </div>
+            <div class="container">
+                <div class="row d-flex">
+                    <div class="col d-flex">
+                        <img src="/img/Team-01.jpg" alt="">
+                        <h4>FABRICIO GUERRA</h4>
+                        <span>Main director</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/Team-02.jpg" alt="">
+                        <h4>UMBERTO PAGODA</h4>
+                        <span>Main writer</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/Team-03.jpg" alt="">
+                        <h4>GLORIA RICCASSO</h4>
+                        <span>Main producer</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/Team-04.jpg" alt="">
+                        <h4>LIO NAPOIN</h4>
+                        <span>Cameraman</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- /Team section -->
+
+        <!-- Quotes section -->
+        <section id="quotes" class="d-flex">
+
+            <div class="top-img">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 979.9 762.6">
+                    <path
+                        d="M266.4,391.1c-37,0-67.8,8.4-92.3,25.9-7,4.9-13.3,7.7-16.8,7.7-4.2,0-7.7-2.1-10.5-7-4.9-7-7-17.5-7-32.2q0-100.8,67.2-193.1C251.8,130.1,314.1,84.7,393.1,56V0Q188.5,74.4,94.4,195.8,0,317.45,0,471.4c0,88.2,23.1,158.8,69.8,212.9,46.2,52.4,101.4,78.3,165.1,78.3,54.6,0,100.7-19.6,139.2-58.1s57.4-83.2,57.4-134.3q0-73.5-50.4-125.9C348.2,408.6,309.8,391.1,266.4,391.1Z"
+                        transform="translate(0 0)"></path>
+                    <path
+                        d="M929.5,444.3c-32.9-35.7-71.3-53.2-114.7-53.2-37,0-67.8,8.4-92.3,25.9-7,4.9-13.3,7.7-16.8,7.7-4.2,0-7.7-2.1-10.5-7-4.9-7-7-17.5-7-32.2q0-100.8,67.2-193.1C800.2,130.1,861.8,84.7,940.8,56V0C804.4,49.6,705.1,114.7,643.4,198.2Q549.05,319.85,549,473.8c0,88.1,23,158,69.2,210.5s101.4,78.3,165.1,78.3c54.6,0,100.7-19.6,139.2-58.1s57.4-83.2,57.4-134.3Q979.9,496.7,929.5,444.3Z"
+                        transform="translate(0 0)"></path>
+                </svg>
+            </div>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum reiciendis magni praesentium animi.
+                Officiis blanditiis distinctio commodi ipsam? Perspiciatis blanditiis labore sed, aut saepe hic illo
+                sunt a est ab!</p>
+            <div class="quote-author d-flex">
+                <img src="/img/01.jpg" alt="">
+                <div class="author-info">
+                    <h4>JOHN DOE</h4>
+                    <span>Designer</span>
+                </div>
+
+            </div>
+            <div>
+                <i class="fa-solid fa-circle"></i>
+                <i class="fa-solid fa-circle"></i>
+                <i class="fa-solid fa-circle"></i>
+            </div>
+
+        </section>
+        <!-- /Quotes section -->
+
+        <!-- News section -->
+        <section id="news" class="p-section">
+            <div class="section-title">
+                <h6>LATEST NEWS</h6>
+                <h2>ARTICLES UPDATED DAILY</h2>
+            </div>
+            <div class="container">
+                <div class="row d-flex">
+                    <div class="col d-flex">
+                        <img src="/img/blog01.jpg" alt="">
+                        <span class="date">November 06 , 2021 By Admin</span>
+                        <h4>RECAP YOUR LATEST COMPANY EVENT</h4>
+                        <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit, ex.</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/blog02.jpg" alt="">
+                        <span class="date">May 27 , 2022 By Admin</span>
+                        <h4>HOW TO EDIT A FILM SCORE TO SERVE</h4>
+                        <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit, ex.</span>
+                    </div>
+                    <div class="col d-flex">
+                        <img src="/img/blog06.jpg" alt="">
+                        <span class="date">May 07 , 2022 By Admin</span>
+                        <h4>ARE YOU READY TO MAKE IT AWESOME</h4>
+                        <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit, ex.</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- /News section -->
+
+        <!-- Partner section -->
+        <section id="partner" class="p-section">
+            <div class="container">
+                <div class="row d-flex">
+                    <div class="col">
+                        <img src="/img/01.png" alt="">
+                    </div>
+                    <div class="col">
+                        <img src="/img/02.png" alt="">
+                    </div>
+                    <div class="col">
+                        <img src="/img/03.png" alt="">
+                    </div>
+                    <div class="col">
+                        <img src="/img/04.png" alt="">
+                    </div>
+                    <div class="col">
+                        <img src="/img/05.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- /Partner section -->
+
+        <!-- Work-with-us section -->
+        <section id="work-with-us" class="p-section">
+            <div class="container d-flex">
+                <div class="col message">
+                    <div class="section-title">
+                        <h6>COLLABORATE</h6>
+                        <h2>
+                            WANT TO WORK WITH US? <br>
+                            LEAVE US A MESSAGE!
+                        </h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolorem asperiores
+                            necessitatibus quaerat? Fuga magnam quaerat deserunt ducimus aut, nesciunt provident esse
+                            perspiciatis cumque? Laboriosam reprehenderit rem eligendi vero autem.
+                        </p>
+                    </div>
+                </div>
+                <form class="col d-flex">
+                    <input type="text" name="full-name" id="full-name" placeholder="Your name">
+
+                    <input type="email" name="mail" id="mail" placeholder="Your email">
+
+                    <input type="number" name="phone" id="phone" placeholder="Mobile number">
+
+                    <input type="password" name="password" id="password" placeholder="Your pincode">
+
+                    <textarea name="message" id="message" cols="30" rows="10"
+                        placeholder="Enter your message"></textarea>
+                    <button type="submit">SUBMIT NOW</button>
+
+                </form>
+            </div>
+        </section>
+        <!-- /Work-with-us section -->
+
+    </div>
+
+</template>
+
+
+
+<style>
+#site-main {
+
+    margin: 0 5px;
+
+
+
+}
+</style>
