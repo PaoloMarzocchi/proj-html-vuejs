7c3986ec4a1bfc756d4fafd47ae485db55c85b94
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-04-10 13:10:50 +0200

    add dynamic header

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,64 @@ export default {
         AppMain,
         AppFooter,
     },
+    data() {
+        return {
+            navBar: {
+                logo: 'logo.svg',
+                menu: [
+                    {
+                        name: 'HOME',
+                        dropDownMenu: null,
+                    },
+                    {
+                        name: 'BLOG',
+                        dropDownMenu: null,
+                    },
+                    {
+                        name: 'PAGES',
+                        dropDownMenu: {
+                            arrowDownImg: 'image (7).svg',
+                            dropdown: ['SERVICES', 'OUR WORK FLOW', 'VIDEO THUMBNAIL TYPE']
+                        },
+                    },
+                    {
+                        name: 'ABOUT',
+                        dropDownMenu: null,
+                    },
+                    {
+                        name: 'CONTACTS',
+                        dropDownMenu: null,
+                    },
+                    {
+                        name: 'GALLERY',
+                        dropDownMenu: null,
+                    },
+                    {
+                        name: 'SHOP',
+                        dropDownMenu: {
+                            arrowDownImg: 'image (7).svg',
+                            dropdown: ['SERVICES', 'OUR WORK FLOW', 'VIDEO THUMBNAIL TYPE']
+                        },
+                    },
+                ],
+                icons: {
+                    search: 'image (8).svg',
+                    social: {
+                        facebook: 'image (9).svg',
+                        twitter: 'image (10).svg',
+                        youtube: 'image (11).svg',
+                        instagram: 'image (12).svg',
+                    }
+
+                }
+            },
+        }
+    },
 }
 </script>
 
 <template>
-    <AppHeader></AppHeader>
+    <AppHeader :logo="navBar.logo" :menu="navBar.menu" :icons="navBar.icons"></AppHeader>
 
     <AppMain></AppMain>
 
